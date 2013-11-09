6aaa544e79bd5877249ad1c015fb8130785a6508
ton <tonton1517@gmail.com>
2013-11-09 16:35:52 +0900

    add FilesParser

--- git_ls_date.py
+++ git_ls_date.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from subprocess import Popen, PIPE
 import sys
@@ -50,6 +51,42 @@ class Commit(object):
 
     def __str__(self):
         return "%s %s " % (self.date, self.hash)
+
+class FilesParser(object):
+    """FilesParser run 'git ls-files' and parse."""
+
+    def __init__(self, pathes = []):
+        self.pathes = pathes if type(pathes) is list else [pathes]
+
+        args = " ".join(["\'%s\'" % f for f in self.pathes])
+        self.files = git("ls-files "+args).split("\n")[:-1]
+        self.files_full = git("ls-files --full-name "+args).split("\n")[:-1]
+
+        self.__abbrev_to_full = {}
+        self.__full_to_abbrev = {}
+
+        for i, f in enumerate(self.files):
+            full = self.files_full[i]
+            self.__abbrev_to_full[f] = full
+            self.__abbrev_to_full[full] = full
+            self.__full_to_abbrev[full] = f
+            self.__full_to_abbrev[f] = f
+
+    def get_full(self, file):
+        """ return full path.
+        Arg : filename
+        Return : return full path. if no result, return None.
+        """
+
+        return self.__abbrev_to_full.get(file)
+
+    def get_abbrev(self, full_path):
+        """ return abbrev path.
+        Arg : full path string
+        Return : return abbrev path. if no result, return None.
+        """
+
+        return self.__full_to_abbrev.get(full_path)
 
 #=======================================
 # main
